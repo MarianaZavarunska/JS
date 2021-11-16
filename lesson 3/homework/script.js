062fc4233f47ec17b2b15d4064ca5836277a95ab
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-11-16 21:30:36 +0200

    classwork was added

--- lesson 3/homework/script.js
+++ lesson 3/homework/script.js
@@ -149,7 +149,7 @@ document.write(`<br>`);
 console.log('=========================Task 17 =========================');
 
 for(let i = 1; i <= 100; i++){
-    if( (i % 2) != 0) {
+    if( (i % 2) !== 0) {
     console.log(`${i}`);
     document.write(`${i} <br>`);
     }
