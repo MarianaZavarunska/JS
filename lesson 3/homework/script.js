a0421a641396d358c9ffde94b70961e1bedc16b5
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-11-16 10:56:55 +0200

    homework was done

--- lesson 3/homework/script.js
+++ lesson 3/homework/script.js
@@ -5,7 +5,7 @@ let numbers = [0, 1, 2, -5, 6];
 let strings = [ 'Ross', 'Rachel', 'Monica', 'Chandler', 'Phoebe', 'Joey'];
 let diverse = [ 'Potter', 1, 7, true, 'wizard'];
 
-console.log(numbers);
+console.log(numbers.toString());
 console.log(strings);
 console.log(diverse);
 
@@ -63,3 +63,94 @@ for( actor of top10){
     console.log(actor);
 }
 
+// Task 9-11
+console.log('=========================Task 9-11 =========================');
+
+let different = [1, true, 'Jonh', -345, false, 'Bob', true, 60, false, 'Mary'];
+
+for (let i = 0; i < different.length; i++) {
+    if (typeof different[i] === 'boolean'){
+        console.log(different[i] ,`${i}`);
+    }
+}
+
+for (let i = 0; i < different.length; i++) {
+    if (typeof different[i] === 'number'){
+        console.log(different[i],`${i}`);
+    }
+}
+
+for (let i = 0; i < different.length; i++) {
+    if (typeof different[i] === 'string'){
+        console.log(different[i],`${i}`);
+    }
+}
+
+// Task 12
+console.log('=========================Task 12 =========================');
+
+let mess = new Array();
+
+mess.push('Andrii');
+mess.push('Ann');
+mess.push(144);
+mess.push(169);
+mess.push(256);
+mess.push(' ');
+mess.push(true);
+mess.push(false);
+mess.push({name: 'Andrii', age: 18});
+mess.push(undefined);
+
+for(const el of mess){
+    console.log(el);
+}
+
+
+// Task 13
+console.log('=========================Task 13 =========================');
+
+for(let i = 0; i < 10; i++){
+    console.log(`${i}`);
+    document.write(`${i} <br>`);
+}
+document.write(`<br>`);
+
+// Task 14
+console.log('=========================Task 14 =========================');
+
+for(let i = 1; i <= 100; i++){
+    console.log(`${i}`);
+    document.write(`${i} <br>`);
+}
+document.write(`<br>`);
+
+// Task 15
+console.log('=========================Task 15 =========================');
+
+for(let i = 1; i <= 100; i+=2){
+    console.log(`${i}`);
+    document.write(`${i} <br>`);
+}
+document.write(`<br>`);
+
+// Task 16
+console.log('=========================Task 16 =========================');
+
+for(let i = 1; i <= 100; i++){
+    if( (i % 2) == 0) {
+    console.log(`${i}`);
+    document.write(`${i} <br>`);
+    }
+}
+document.write(`<br>`);
+
+// Task 17
+console.log('=========================Task 17 =========================');
+
+for(let i = 1; i <= 100; i++){
+    if( (i % 2) != 0) {
+    console.log(`${i}`);
+    document.write(`${i} <br>`);
+    }
+}
