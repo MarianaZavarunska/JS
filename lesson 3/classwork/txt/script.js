c39d7b9f0a57d6e925765ac2c44f599211f08461
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-11-17 00:18:53 +0200

    classwork was done

---
+++ lesson 3/classwork/txt/script.js
@@ -0,0 +1,105 @@
+
+let numbers = [2,17,13,6,22,31,45,66,100,-18];
+
+// Task 1
+console.log('=========================Task 1=========================');
+
+let i = 0; 
+ while( i < numbers.length) {
+
+    i++;
+ }
+
+ // Task 2
+console.log('=========================Task 2=========================');
+ 
+for( let numb of numbers) { 
+
+} 
+
+// Task 3
+console.log('=========================Task 3=========================');
+
+let j = 0; 
+let odds = [];
+
+ while( j < numbers.length) {
+    if( j % 2 !== 0 ){
+       odds.push(numbers[j]);
+    }
+    j++;
+ }
+ console.log(numbers);
+ console.log(odds);
+
+ // Task 4
+console.log('=========================Task 4 =========================');
+
+odds = [];
+
+for(let i = 0; i < numbers.length; i++) { 
+    if( i % 2 !== 0 ){
+        odds.push(numbers[i]);
+     }
+} 
+console.log(numbers);
+console.log(odds);
+
+// Variant 2
+console.log('========================= Variant 2 =========================');
+
+odds = [];
+for(let i = 1; i < numbers.length; i+=2) { 
+    odds.push(numbers[i]); 
+} 
+console.log(odds);
+
+ // Task 5
+ console.log('=========================Task 5 =========================');
+
+j = 0; 
+odds = [];
+
+ while( j < numbers.length) {
+    if( j % 2 == 0 ){
+       odds.push(numbers[j]);
+    }
+    j++;
+ }
+ console.log(numbers);
+ console.log(odds);
+
+// Task 6
+console.log('=========================Task 6 =========================');
+
+odds = [];
+
+for(let i = 0; i < numbers.length; i++) { 
+    if( i % 2 == 0 ){
+        odds.push(numbers[i]);
+     }
+} 
+console.log(numbers);
+console.log(odds);
+
+// Task 6
+console.log('=========================Task 6 =========================');
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 === 0) {
+        numbers[i] = 'okten';
+    }
+}
+console.log(numbers);
+
+// Task 7
+console.log('=========================Task 7 =========================');
+
+odds = [];
+for( let i = numbers.length - 1; i >= 0; i--){
+    odds.push(numbers[i]); 
+}
+console.log(odds);
+
+// console.log(numbers.reverse());
+
