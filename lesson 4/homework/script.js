41a3ac6e3120a943827a0a4f3a7ad81945923b0c
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-11-18 01:18:43 +0200

    homework was done

---
+++ lesson 4/homework/script.js
@@ -0,0 +1,103 @@
+// Task 1
+console.log('=========================Task 1=========================');
+
+function findAreaRectangle(a,b){
+    return a * b;
+}
+
+let area = findAreaRectangle(10,3);
+console.log(area);
+
+// Task 2
+console.log('=========================Task 2=========================');
+
+let circleArea = (r) => Math.PI * Math.pow(r,2);
+let showMessage = (r) => `The area of circle with r = ${r} is ${circleArea(r)}`;
+
+console.log(showMessage(5));
+
+// Task 3
+console.log('=========================Task 3=========================');
+
+function findAreaCylinder(r,h){
+    return 2 * findMainArea(r) + findAsideArea(r,h);
+}
+
+function findMainArea(r) {
+     return Math.PI *  Math.pow(r,2);
+}
+
+function findAsideArea(r,h) {
+    return 2 * Math.PI * r * h;
+}
+
+console.log(findAreaCylinder(2,5))
+
+
+// Task 4
+console.log('=========================Task 4========================='); 
+
+let actors = ['Will Smith', 'Ryan Reynolds', 'Ben Stiller', 'Patrick Dempsey', 'Morgan Freeman', 'Dwayne Johnson', 'Adam Sandler', 'Arnold Schwarzenegger', 'Robert De Niro', 'Sylvester Stallone'];
+
+showActor(actors);
+
+function showActor(actors){
+    actors.forEach(actor => {
+        console.log(actor);
+    });
+}
+
+// Task 5
+console.log('=========================Task 5========================='); 
+
+showParagraph('Ryan Reynolds is a Canadian-born Canadian-American actor and film producer. He began his career starring in the Canadian teen soap opera Hillside');
+
+function showParagraph(text) {
+    document.write(`<p class="info"> ${text}</p>`);
+}
+
+
+// Task 6
+console.log('=========================Task 6========================='); 
+
+createList('Will Smith');
+
+function createList(text) {
+   document.write('<ul class="actors">');
+   for(i = 0; i < 3; i++){
+       document.write(`<li>${text}</li>`)
+   }
+
+   document.write('</ul>');
+}
+
+
+// Task 7
+console.log('=========================Task 7========================='); 
+
+createListWithSize('Patrick Dempsey', 5);
+
+function createListWithSize(text, amoung) {
+    document.write('<ul class="actors">');
+    for(i = 0; i < amoung; i++){
+        document.write(`<li> ${text} </li>`)
+    }
+    document.write('</ul>');
+ }
+
+ // Task 8
+console.log('=========================Task 8========================='); 
+
+let friends = [ 'Ross', 'Rachel', 'Monica', 'Chandler', 'Phoebe', 'Joey'];
+
+createListOfArrayElement(friends);
+
+function createListOfArrayElement(friends){
+    document.write('<ul class="friends-container">');
+    for(i = 0; i < friends.length; i++){
+        document.write(`<li> ${friends[i]} </li>`)
+    }
+    document.write('</ul>');
+
+}
+ 
