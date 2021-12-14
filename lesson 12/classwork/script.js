bc859e960948c62afd0579a1f2f5ddf80740d3f7
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2021-12-14 14:06:10 +0200

    one more 'fetch request' was added

--- lesson 12/classwork/script.js
+++ lesson 12/classwork/script.js
@@ -11,21 +11,24 @@ fetch('https://jsonplaceholder.typicode.com/posts')
             let divPost = document.createElement('div');
             divPost.classList.add('post-container');
             let button = document.createElement('button');
-            button.id = 'load-post';
+            button.className = 'load-post';
             button.innerText = 'Show Post'
 
             for (const key in post) {
+                if (key === 'body') continue;
                 let divKey = document.createElement('div');
                 divKey.classList.add('key-container');
-
-                divKey.innerText = `${key}:` + " " + `${post[key]}`;
-                if (key === 'body') {
-                    divKey.id = 'post-content';
-
-                    button.addEventListener('click', () => {
-                        divKey.classList.toggle('active');
-                    })
+                if (key === 'id') {
+                    divKey.setAttribute('user-id', post[key]);
                 }
+                divKey.innerText = `${key}:` + " " + `${post[key]}`;
+                // if (key === 'body') {
+                //     divKey.id = 'post-content';
+
+                //     button.addEventListener('click', () => {
+                //         divKey.classList.toggle('active');
+                //     })
+                // }
 
                 divPost.append(divKey);
             }
@@ -33,7 +36,28 @@ fetch('https://jsonplaceholder.typicode.com/posts')
             divPost.append(button);
             document.body.append(divPost);
         }
-    })
 
+        const btns = document.getElementsByClassName('load-post');
+
+        for (const btn of btns) {
+            btn.addEventListener('click', () => {
+                
+                if(btn.parentElement.childElementCount === 5) return;
+
+                let number = btn.parentElement.querySelector('div[user-id]').getAttribute('user-id');
+                fetch(`https://jsonplaceholder.typicode.com/posts/${number}`)
+                    .then(response => response.json())
+                    .then(response => {
+
+                        let div = document.createElement('div')
+                        div.classList.add('post-content');
+
+                        div.innerText = `body: ${response['body']}`
+                        btn.parentElement.insertBefore(div, btn);
+                    })
+            })
+        }
+
+    })
 
 
